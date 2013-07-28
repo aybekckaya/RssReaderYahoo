514fb3c550339b171b1dcfcf4dfe26290793c8aa
aybekckaya <aybekcankaya@icloud.com>
2013-07-28 06:18:57 +0300

    Initial Commit

---
+++ RssReaderYahooTests/RssReaderYahooTests.h
@@ -0,0 +1,13 @@
+//
+//  RssReaderYahooTests.h
+//  RssReaderYahooTests
+//
+//  Created by aybek can kaya on 7/28/13.
+//  Copyright (c) 2013 aybek can kaya. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RssReaderYahooTests : SenTestCase
+
+@end
---
+++ RssReaderYahooTests/RssReaderYahooTests.m
@@ -0,0 +1,32 @@
+//
+//  RssReaderYahooTests.m
+//  RssReaderYahooTests
+//
+//  Created by aybek can kaya on 7/28/13.
+//  Copyright (c) 2013 aybek can kaya. All rights reserved.
+//
+
+#import "RssReaderYahooTests.h"
+
+@implementation RssReaderYahooTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RssReaderYahooTests");
+}
+
+@end
