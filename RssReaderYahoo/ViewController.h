94bc6a1679a52ca1976ae8309a709259338bfb0b
aybekckaya <aybekcankaya@icloud.com>
2013-07-28 06:23:04 +0300

    1.commit

--- RssReaderYahoo/ViewController.h
+++ RssReaderYahoo/ViewController.h
@@ -1,13 +1,26 @@
 //
 //  ViewController.h
-//  RssReaderYahoo
+//  YahooRssReader
 //
 //  Created by aybek can kaya on 7/28/13.
 //  Copyright (c) 2013 aybek can kaya. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "Parser.h"
+#import "MBProgressHUD.h"
+#import "RssCellCL.h"
 
-@interface ViewController : UIViewController
+/*
+    - Internet baglantisi olmadigi zamanki test 
+ */
+
+@interface ViewController : UIViewController<ParserDelegate,MBProgressHUDDelegate,UITableViewDataSource,UITableViewDelegate,RssCellDelegate>
+{
+    Parser *XmlParser;
+    MBProgressHUD *CHUD;
+    NSMutableArray *RssItems;
+    int currentSelection;
+}
 
 @end
--- RssReaderYahoo/ViewController.m
+++ RssReaderYahoo/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  RssReaderYahoo
+//  YahooRssReader
 //
 //  Created by aybek can kaya on 7/28/13.
 //  Copyright (c) 2013 aybek can kaya. All rights reserved.
@@ -18,7 +18,101 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    CHUD=[[MBProgressHUD alloc]initWithView:self.view];
+    [self.view addSubview:CHUD];
+    CHUD.delegate=self;
+    [CHUD show:YES];
+    
+    currentSelection=-1;
+    
+    RssItems=[[NSMutableArray alloc]init];
+    XmlParser =[Parser SharedParser];
+    XmlParser.delegate=self;
+    [XmlParser ParseXmlAtURL:@"http://news.yahoo.com/rss/"];
+    
+    
 }
+
+
+#pragma mark ParserDelegate
+
+
+-(void)ParserDidFetchRssItems:(NSArray *)RssArr
+{
+    [XmlParser CreateEntriesFromRssItems:0]; // init callas
+}
+
+
+-(void)ParserDidEndCreatingEntryObjects:(NSMutableArray *)EntryObjects
+{
+    [RssItems addObjectsFromArray:EntryObjects];
+    
+    [CHUD hide:YES];
+}
+
+#pragma mark ParserDelegate END
+
+
+
+#pragma mark TableViewDelegates
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    [RssItems count];
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    const NSInteger TABLECELLTAG=1000;
+    
+    static NSString *reuseIdentifier = @"Cell";
+    //asdeewe
+    RssCellCL *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
+    
+    if(cell == nil)
+    {
+        NSArray* views = [[NSBundle mainBundle] loadNibNamed:@"RssCell" owner:nil options:nil];
+        
+        cell =(RssCellCL *)[views objectAtIndex:0];
+        cell.delegate=self;
+        //asdawe
+        
+    }
+    else
+    {
+        // cell has created Beforej
+    }
+    
+    Entry *theEntry=[RssItems objectAtIndex:indexPath.row];
+    [cell SetNewsLblText:[theEntry description]];
+    [cell SetHeaderLblText:[theEntry title]];
+    [cell SetEntryImageStr:[theEntry imageStr]];
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+#pragma mark TableViewDelegates END
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
